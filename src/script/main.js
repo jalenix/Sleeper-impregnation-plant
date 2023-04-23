9b48ea0152cdc60d5a3eca75d8bced9f355a1865
jalenix <babaevwtf@mail.ru>
2023-04-23 14:27:20 +0300

    add calculate

--- src/script/main.js
+++ src/script/main.js
@@ -1,8 +1,22 @@
-class people {
-    sayhi() {
-        console.log('Hi')
-    }
-}
+const rangeWay = document.getElementById('rangeWay')
+const amount = document.getElementById('amount')
+const result = document.getElementById('result')
 
-let b = new people()
-b.sayhi()
+function count() {
+    const wayValue = rangeWay.value
+    const amountValue = amount.value
+    // console.log(wayValue,' ', amountValue)
+    const resultValue = (amountValue / 1000) * wayValue
+    result.textContent = resultValue
+}
+document.addEventListener("DOMContentLoaded", function (event) {
+    count();
+});
+
+
+rangeWay.addEventListener('input', function () {
+    count()
+})
+amount.addEventListener('change', function () {
+    count()
+})
